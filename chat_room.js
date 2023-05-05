d72b4db5aedd6aa8f9e9f006b74ec73cc75f0148
Kartik8275 <111053135+Kartik8275@users.noreply.github.com>
2023-05-05 15:36:49 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,37 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqXWXJii4Un9XPvZTpvsLmt2evh3hsK54",
+    authDomain: "room-name-5f71c.firebaseapp.com",
+    projectId: "room-name-5f71c",
+    storageBucket: "room-name-5f71c.appspot.com",
+    messagingSenderId: "768185140191",
+    appId: "1:768185140191:web:423079e07d6eab052b3a6a",
+    measurementId: "G-2M35PNJ41B"
+  };
+  function getData() {firebase.database().ref("/").on('value',
+  function(snapshot) {document.getElementById("output").innerHTML = 
+  "";snapshot.forEach(function(childSnapshot)  {childkey = 
+  childSnapshot.key;
+  Room_name = childkey;
+  <div>
+    string x =["ABCD" , "EFGH"];
+    
+  </div>
+  
+  
+  });
+  function redirectToroom(){
+    redirectToroom = firebase.database;
+  }
+
+
+
+
+
+}
+  
+  
+  
+  
+  
+  
+  )}
